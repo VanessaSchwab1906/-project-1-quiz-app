0ba101ca9c3686dbcfe2e55cf1b179ce25ffe5fc
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-03-27 23:14:30 +0200

    added event listener to newly added card at form page

--- form.js
+++ form.js
@@ -9,9 +9,9 @@ form.addEventListener("submit", (event) => {
   const questionValue = formElements.question.value;
   //   console.log(questionValue);
   const answerValue = formElements.answer.value;
-  console.log(answerValue);
+  // console.log(answerValue);
   const tagValue = formElements.tag.value;
-  console.log(tagValue);
+  // console.log(tagValue);
 
   const newSection = document.createElement("section");
   newSection.classList.add("question-card");
@@ -19,23 +19,51 @@ form.addEventListener("submit", (event) => {
     <img
       src="./assets/bookmark_transparent.png"
       alt="bookmark transparent"
-      id="bookmark-1"
-      data-js="bookmark-1"
+      id="bookmark-2"
+      data-js="bookmark-2"
     />
     </section>
     <p>
     ${questionValue} <br />
     </p>
     <section>
-    <button type="button" data-js="button">Show answer</button>
+    <button type="button" data-js="button-new-card">Show answer</button>
     </section>
-    <p hidden data-js="hidden-answer">${answerValue}</p>
+    <p hidden data-js="hidden-answer-new-card">${answerValue}</p>
     <section class="tag-section">
     <a href="#">${tagValue}</a>
     </section>
     `;
-
+  // add card to body
   document.body.append(newSection);
+
+  // toggle function for show answer button
+  const buttonNewCard = document.querySelector('[data-js="button-new-card"]');
+  const hiddenAnswerNewCard = document.querySelector(
+    '[data-js="hidden-answer-new-card"]'
+  );
+
+  buttonNewCard.addEventListener("click", () => {
+    hiddenAnswerNewCard.toggleAttribute("hidden");
+    buttonNewCard.textContent = "Hide answer";
+    if (hiddenAnswerNewCard.hasAttribute("hidden")) {
+      buttonNewCard.textContent = "Show answer";
+    } else {
+      buttonNewCard.textContent = "Hide answer";
+    }
+  });
+
+  // toggle function for bookmark
+  const bookmark = document.querySelector('[data-js="bookmark-2"]');
+  let toggle = true;
+  bookmark.addEventListener("click", () => {
+    toggle = !toggle;
+    if (toggle) {
+      bookmark.setAttribute("src", "./assets/bookmark_transparent.png");
+    } else {
+      bookmark.setAttribute("src", "./assets/bookmark_filled.png");
+    }
+  });
 });
 
 // CHARACTERS COUNT (Question-Area)
